9e0411ac083a2d6af0fc92bf3ed2bf6d171eed6c
Anthony Shuker <anthony@emobe.uk>
2019-10-20 20:10:46 +0100

    fix tsconfig

--- dist/ObservableDictionary.d.ts
+++ dist/ObservableDictionary.d.ts
@@ -1,4 +1,14 @@
-import { Dictionary } from "./Dictionary";
-export declare class ObservableDictionary<K, V> extends Dictionary<K, V> {
-    subscribe(callback: (value: V) => void): void;
+import { Dictionary } from ".";
+export default class ObservableDictionary<K, V> extends Dictionary<K, V> {
+    private subject;
+    items$: import("rxjs").Observable<Map<K, V>>;
+    /**
+     * Adds item to the dictionary
+     * @param key The key which will be used to access the item
+     * @param value The item to add to the dictionary
+     * @returns `true` if item was added to the dictionary, otherwise throws an `Error` if key already exists
+     */
+    add(key: K, value: V): boolean;
+    remove(key: K): boolean;
+    update(): void;
 }
